3ed305334e25d1859d3bc49f9d762d803c3e7c8d
Mj <chenmengchieh2012@gmail.com>
2019-08-31 16:07:50 +0800

    access from tab

--- src/main/kotlin/com/news/controller/NewsController.kt
+++ src/main/kotlin/com/news/controller/NewsController.kt
@@ -4,6 +4,7 @@ import com.news.entity.TabEntity
 import com.news.service.NewsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,9 +22,9 @@ class NewsController {
         return newService.getNews()
     }
 
-//    @GetMapping(value = "/test/tag/hashValue")
-//    fun getTagHashValue(): String?{
-//        return newService.extractTab()
-//    }
+    @GetMapping(value = "/test/tab/{tabName}")
+    fun getTagHashValue(@PathVariable tabName:String): String?{
+        return newService.getTabPage(tabName)
+    }
 
 }
--- src/main/kotlin/com/news/service/NewsService.kt
+++ src/main/kotlin/com/news/service/NewsService.kt
@@ -4,10 +4,9 @@ import com.news.entity.TabEntity
 import org.apache.logging.log4j.kotlin.Logging
 import org.jsoup.Jsoup
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import javax.lang.model.element.Element
+import org.springframework.web.client.getForObject
 
 @Service
 class NewsService : Logging {
@@ -17,6 +16,7 @@ class NewsService : Logging {
     lateinit var restTemplate: RestTemplate
 
     private lateinit var rawNews: String
+
 
     private lateinit var latest: TabEntity
     private lateinit var intenation: TabEntity
@@ -28,30 +28,53 @@ class NewsService : Logging {
 
     private val tabs = listOf("最新")
 
-    private val newsRootUrl: String = "https://news.google.com/?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
+    private val newsGETparameter = "?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
     private val newsHost: String = "https://news.google.com"
+    private var newsIndexPageValue: String? = null
 
-    var context: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
 
-    fun extractTab(rawNews: String): String {
+    private fun getMainPage(rawNews: String): String? {
         val doc = Jsoup.parse(rawNews)
-        val hashdoms = doc.select("a:contains(更多「頭條新聞」相關內容)").forEach {
-            logger.info(it.allElements)
-            return it.attr("href")!!
+        lateinit var hashValue : String
+        doc.select("a:contains(更多「頭條新聞」相關內容)").forEach {
+//            logger.info(it.allElements)
+            val tmpHashValue = it.attr("href")
+            if(!tmpHashValue.equals("Not found")) hashValue = tmpHashValue
         }
-        return "Not found"
+        return hashValue
+    }
+
+    private fun getTagValue(rawNews: String ,tag: String): String? {
+        val doc = Jsoup.parse(rawNews)
+        logger.info(rawNews.length)
+        lateinit var hashValue : String
+        doc.select("span:contains($tag)").forEach {
+//            logger.info(it.allElements)
+            val tmpHashValue = it.parents().parents().parents().attr("aria-controls")
+            logger.info(tmpHashValue)
+            if(tmpHashValue.length != 0) hashValue = tmpHashValue
+        }
+        return hashValue
+    }
+
+    fun getTabPage(tab: String): String? {
+        val newsTabPageUrl = newsIndexPageValue?.let {
+            newsHost + newsIndexPageValue + "/sections/" + getTagValue(rawNews,tab)?.substring(3) + newsGETparameter
+        }
+        newsTabPageUrl?.let { logger.info(it) }
+        return newsTabPageUrl?.let { restTemplate.getForObject(it, String().javaClass) }
     }
 
     fun getNews() : String? {
-        val latestNewsPage = restTemplate.getForObject(newsRootUrl,String().javaClass)!!
-        val newsIndexPageUrl = newsHost + extractTab(latestNewsPage).substring(1)
-        rawNews = restTemplate.getForObject(newsIndexPageUrl,String().javaClass)!!
+        val latestNewsPage = restTemplate.getForObject(newsHost+"/"+newsGETparameter,String().javaClass)
+        this.newsIndexPageValue = latestNewsPage?.let { getMainPage(it)?.substring(1)?.substringBefore("?") }
+        this.rawNews = newsIndexPageValue?.let {
+            restTemplate.getForObject(newsHost + it +"/"+newsGETparameter, String().javaClass)
+        }.toString()
         return rawNews
     }
+
+
 
 
 
